47acca98dcc4c75a8352295b31f1b4e6006267c2
filipestrike <filipe.nogueira2001@gmail.com>
2023-05-10 21:43:46 -0300

    inclusao de acoes e emoções

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 var box1 = document.getElementById("box1");
 box1.addEventListener("click", function () {
-  var audio = new Audio("som1.mp3");
+  var audio = new Audio("/utils/aud/aud1.mp3");
   audio.play();
 });
 
 var box2 = document.getElementById("box2");
 box2.addEventListener("click", function () {
-  var audio = new Audio("som2.mp3");
+  var audio = new Audio("/utils/aud/aud2.mp3");
   audio.play();
 });
 
 var box3 = document.getElementById("box3");
 box3.addEventListener("click", function () {
-  var audio = new Audio("som3.mp3");
+  var audio = new Audio("/utils/aud/aud3.mp3");
   audio.play();
 });
 
 var box4 = document.getElementById("box4");
 box4.addEventListener("click", function () {
-  var audio = new Audio("som4.mp3");
+  var audio = new Audio("/utils/aud/aud4.mp3");
   audio.play();
 });
